922b9eae91120e8d798157e04e701bb6ddcd35b6
Esmeralda <elara294jiutepec@gmail.com>
2022-12-05 18:52:55 -0600

    initial commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon\Date;
+use Illuminate\Support\Facades\DB;
+
+class BookController extends Controller
+{
+
+    public function BookController()
+    {
+    }
+
+    public function index()
+    {
+        // $books = Book::all();
+        $books = Book::with('authors', 'category', 'editorial')->get();
+        return [
+            "error" => false,
+            "message" => "Successfull query",
+            "data" => $books
+        ];
+    }
+
+    public function show($id)
+    {
+        $response = $this->getReponse();
+        $book = Book::with('category', 'editorial', 'authors')->where("id", $id)->get();
+        if ($book) {
+            $response["error"] = false;
+            $response["message"] = "Successfull query!";
+            $response["data"] = $book;
+        } else {
+            $response["message"] = "Not found";
+        }
+        return $response;
+    }
+
+    public function delete($id)
+    {
+        $response = $this->getResponse();
+        $book = Book::find($id);
+
+        if ($book) {
+            foreach ($book->authors as $item) {
+                $book->authors()->detach($item->id);
+            }
+            $book->delete();
+            $response["error"] = false;
+            $response["message"] = "Your book has been removed!";
+            $response["data"] = $book;
+        } else {
+            $response["message"] = "Not found";
+        }
+        return $response;
+    }
+
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+
+        try {
+
+            $existIsbn = Book::where("isbn", trim($request->isbn))->exists();
+
+            if (!$existIsbn) {
+                $book = new Book();
+                $book->isbn = trim($request->isbn);
+                $book->title = trim($request->title);
+                $book->description = trim($request->description);
+                $book->category_id = $request->category['id'];
+                $book->editorial_id = $request->editorial['id'];
+                $book->publish_date = Carbon::now();
+                $book->save();
+
+                foreach ($request->authors as $item) {
+                    $book->authors()->attach($item);
+                }
+
+                $bookId = $book->id;
+                return [
+                    "status" => true,
+                    "message" => "Your book has been created!",
+                    "data" => [
+                        "book_id" => $bookId,
+                        "book" => $book
+                    ]
+                ];
+            } else {
+                return [
+                    "error" => true,
+                    "message" => "The isbn already exist",
+                    "data" => []
+                ];
+            }
+
+            DB::commit(); //Save all
+        } catch (Exception $e) {
+            DB::rollBack(); //Revert all
+            return [
+                "error" => true,
+                "message" => "Wrong operation!",
+                "data" => []
+            ];
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        DB::beginTransaction();
+
+        try {
+            $response = $this->getResponse();
+            $book = Book::find($id);
+
+            if ($book) {
+
+                $isbnOwner = Book::where("isbn", $request->isbn)->first();
+
+                if (!$isbnOwner || $isbnOwner->id == $book->id) {
+
+                    $book->isbn = trim($request->isbn);
+                    $book->title = trim($request->title);
+                    $book->description = trim($request->description);
+                    $book->category_id = $request->category['id'];
+                    $book->editorial_id = $request->editorial['id'];
+                    $book->publish_date = Carbon::now();
+                    $book->update();
+
+                    //Delete old authors
+                    foreach ($book->authors as $item) {
+                        $book->authors()->detach($item->id);
+                    }
+
+                    //Add new authors
+                    foreach ($request->authors as $item) {
+                        $book->authors()->attach($item);
+                    }
+
+                    $book = Book::with('authors', 'category', 'editorial')->where("id", $id)->get();
+                    $response["data"] = $book;
+                    $response["error"] = false;
+                    $response["message"] = "Your bok has been updated!";
+                } else {
+                    $response["message"] = "ISBN DUPLICATED";
+                }
+            } else {
+
+                $response["error"] = true;
+                $response["message"] = "NOT FOUND";
+            }
+
+            DB::commit();
+            return $response;
+
+        }catch(Exception $e){
+            DB::rollBack(); //Revert all
+            return [
+                "error" => true,
+                "message" => "Wrong operation!",
+                "data" => $e
+            ];
+        }
+
+
+    }
+}
---
+++ database/migrations/2022_11_10_005428_create_book_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {/*
+        Schema::create('book_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment',150);
+            $table->boolean('edited');
+            $table->timestamps();
+        });*/
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_reviews');
+    }
+}
